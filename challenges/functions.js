4fc3318793b089a475b15911fd5008d935fca367
Jonathan Canela <jonathancanela365@gmail.com>
2020-05-02 11:17:06 -0700

    Finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,26 +6,39 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
-
+function consume(a, b, cb){
+  return cb(a, b);
+}
+function callback(item1, item2){
+  return 'This is ' + item1 + ' and this is ' + item2;
+}
+console.log(consume('alignment', 'disalignment', callback))
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
-
+function add(a , b){
+  return a + b;
+}
+function multiply(a, b){
+  return a * b;
+}
+function greeting(firstName, lastName){
+  return 'Hello ' + firstName + ' ' + lastName + ', nice to meet you!'
+}
+console.log(consume('Jonathan', 'Canela', greeting))
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: It can access it because the variable has a block scope which means it can be used in the function/box it's created in.
 
 
 const external = "I'm outside the function";
